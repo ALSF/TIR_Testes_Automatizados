daa1612911de5cd1e5c0ce78a34e784ee1909c5b
Fabio Henrique Favaretto <fhfavaretto@alsf.local>
2020-11-13 10:26:13 -0300

    usuário pergunta no terminal

--- operacional/usuário_reset.py
+++ operacional/usuário_reset.py
@@ -4,16 +4,19 @@ from datetime import datetime
 import time
 dataSystem = datetime.today().strftime('%d/%m/%y')
 
+user = input("Digite o nome do usuário: ") #usuario informar 
+pwd = input("Digite a nova senha:  ") # informar senha 
+
 oHelper = Webapp()
 oHelper.Setup("SIGAMDI", dataSystem, '01','11001','02')
-oHelper.SetLateralMenu(' Miscelanea (15) > Usuários ')
+oHelper.SetLateralMenu(' Miscelanea > Usuários ')
 oHelper.SetButton("Confirmar")
 oHelper.SetButton("Não")
-oHelper.SearchBrowse(term="dtcassavara", key=2, index=True)
+oHelper.SearchBrowse(term=user, key=2, index=True)
 oHelper.SetButton("Alterar")
-oHelper.SetValue("USR_PSW","lar004")
-oHelper.SetValue("USR_PSWCMP","lar004")
-oHelper.SetValue("USR_MSBLQL", "2 - Não")
-oHelper.SetValue("USR_CHGPSW", True, name_attr=True)
+oHelper.SetValue("USR_PSW", pwd)
+oHelper.SetValue("USR_PSWCMP", pwd)
+oHelper.SetValue("USR_MSBLQL", "2 - Não") # Para bloquear para desbloquear > "2 - Não"
+oHelper.SetValue("USR_CHGPSW", True, name_attr=True) #Para trocar a senha após logar
 oHelper.SetButton("Confirmar")
 oHelper.SetButton("Fechar")
